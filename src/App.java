9b1fbcca1225befd824362fffa3e139c2b1383d5
Katona Valentin <djvalee.yt@gmail.com>
2021-09-06 09:09:44 +0200

    Init

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/*
+* File: App.java
+* Author: Katona Valentin
+* Copyright: 2021, Katona Valentin
+* Group: Szoft II/N
+* Date: 2021-09-06
+* Github: https://github.com/djvalee
+* Licenc: GNU GPL
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
